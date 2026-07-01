0ce308465e4b8eb3dbcb730d7909f13d520beef4
sakha82 <musk@dsb.dk>
2026-07-01 09:32:10 +0200

    latest-wmsapp21

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -11,9 +11,9 @@ export const environment = {
     production: false,
     // BASE_URL:'https://wmstestapi-addgaegmecamcyb4.westeurope-01.azurewebsites.net',
     // BASE_URL:'https://wmsapi-dhexdwcheaejf0gr.swedencentral-01.azurewebsites.net',
-          BASE_URL:'https://wmsapi-dhexdwcheaejf0gr.swedencentral-01.azurewebsites.net',
+     //     BASE_URL:'https://wmsapi-dhexdwcheaejf0gr.swedencentral-01.azurewebsites.net',
 
-    // BASE_URL:'https://localhost:7061',
+    BASE_URL:'http://localhost:5000',
     logLevel: LOG_LEVEL.debug,
     Version:'2.13T'
   };
